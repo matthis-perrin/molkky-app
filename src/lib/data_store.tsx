2e6d93ee18a93b1f88a87222fec6fbe2f5a6442d
Matthis Perrin <matthis.perrin@gmail.com>
2021-09-19 23:45:16 +0200

    Update dependency for AsyncStorage

--- src/lib/data_store.tsx
+++ src/lib/data_store.tsx
@@ -1,5 +1,5 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useEffect, useState} from 'react';
-import {AsyncStorage} from 'react-native';
 
 interface DataStoreApi<GetData, SetData> {
   getData: () => GetData;
